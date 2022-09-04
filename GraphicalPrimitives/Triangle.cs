a303fa18781a7b0f5e768d95c279ee1006dafc13
MaxEpt <mak_sim93@mail.ru>
2022-09-04 22:07:01 +0300

    Triangle

--- GraphicalPrimitives/Triangle.cs
+++ GraphicalPrimitives/Triangle.cs
@@ -14,18 +14,38 @@ namespace GraphicalPrimitives
 
         public double SideCLength;
 
-        public bool IsRectangular { get => false; }
-
-        public Triangle(double sideALength, double sidebLength, double sideCLength)
+        public bool IsRectangular
         {
+            get 
+            {
+                if (SideALength > SideBLength && SideALength > SideCLength)
+                    return Math.Round(Math.Pow(SideALength, 2), 3) == Math.Round(Math.Pow(SideBLength, 2) + Math.Pow(SideCLength, 2), 3);
+                else if (SideBLength > SideCLength)
+                    return Math.Round(Math.Pow(SideBLength, 2), 3) == Math.Round(Math.Pow(SideALength, 2) + Math.Pow(SideCLength, 2), 3);
+                else
+                    return Math.Round(Math.Pow(SideCLength, 2), 3) == Math.Round(Math.Pow(SideALength, 2) + Math.Pow(SideBLength, 2), 3);
+            }
+        }
+
+        public Triangle(double sideALength, double sideBLength, double sideCLength)
+        {
+            if (!(sideALength + sideBLength > sideCLength) ||
+                !(sideALength + sideCLength > sideBLength) ||
+                !(sideBLength + sideCLength > sideALength))
+            {
+                throw new ArgumentException("Sides length is incorrect");
+            }
+
             SideALength = sideALength;
-            SideBLength = sidebLength;
+            SideBLength = sideBLength;
             SideCLength = sideCLength;
         }
 
         public override double CalculateArea()
         {
-            return 0;
+            double halfPerimeter = (SideALength + SideBLength + SideCLength) / 2;
+
+            return Math.Sqrt(halfPerimeter * (halfPerimeter - SideALength) * (halfPerimeter - SideBLength) * (halfPerimeter - SideCLength));
         }
     }
 }
