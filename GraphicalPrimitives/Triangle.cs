2412e2967ec47edc1910abce4f2f9373da1e4251
MaxEpt <mak_sim93@mail.ru>
2022-09-04 17:18:01 +0300

    triangle class

---
+++ GraphicalPrimitives/Triangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicalPrimitives
+{
+    public class Triangle: GraphicalPrimitive
+    {
+        public double SideALength;
+
+        public double SideBLength;
+
+        public double SideCLength;
+
+        public bool IsRectangular { get => false; }
+
+        public Triangle(double sideALength, double sidebLength, double sideCLength)
+        {
+            SideALength = sideALength;
+            SideBLength = sidebLength;
+            SideCLength = sideCLength;
+        }
+
+        public override double CalculateArea()
+        {
+            return 0;
+        }
+    }
+}
