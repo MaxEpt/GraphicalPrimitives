c9c8367864f7772a5cf9816fbd6e81857f49a3cc
MaxEpt <mak_sim93@mail.ru>
2022-09-04 22:14:22 +0300

    Example project

---
+++ Example/Circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphicalPrimitives;
+
+namespace Example
+{
+    internal class Circle : GraphicalPrimitive
+    {
+        public double Radius;
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;    
+        }
+    }
+}
---
+++ Example/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GraphicalPrimitives;
+using Example;
+
+List<GraphicalPrimitive> primitives = new List<GraphicalPrimitive>();
+
+var triangle = new Triangle(2, 2, 2);
+primitives.Add(triangle);
+
+var circle = new Circle(10);
+primitives.Add(circle);
+
+foreach (var primitive in primitives)
+{
+    Console.WriteLine(primitive.CalculateArea());
+}
