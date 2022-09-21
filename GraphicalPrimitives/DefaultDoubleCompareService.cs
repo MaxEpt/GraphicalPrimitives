615d6baab616d7e3de7c79edd54fc729968d7e93
MaxEpt <mak_sim93@mail.ru>
2022-09-21 22:50:08 +0300

    IDoubleCompareService, DefaultDoubleCompareService

---
+++ GraphicalPrimitives/DefaultDoubleCompareService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicalPrimitives
+{
+    public class DefaultDoubleCompareService : IDoubleCompareService
+    {
+        public bool IsEqual(double a, double b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GraphicalPrimitives/IDoubleCompareService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicalPrimitives
+{
+    public interface IDoubleCompareService
+    {
+        public bool IsEqual(double a, double b);
+    }
+}
